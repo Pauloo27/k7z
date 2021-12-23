83da211b698cd2b516b42b170aea263748c51e68
Paulo <git@pauloo27.dev>
2021-12-23 10:24:10 -0300

    feat: add monitor

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/Pauloo27/k7z/internal/config"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
 func StartHTTPServer() error {
 	app := fiber.New()
+	app.Get("/admin/dashboard", monitor.New())
 	route(app)
 	app.Listen(":" + strconv.Itoa(config.Port))
 	return nil
