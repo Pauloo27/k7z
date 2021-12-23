acc38c0980b650065aeff6a538fb0a3f2822379f
Paulo <git@pauloo27.dev>
2021-12-23 00:20:01 -0300

    feat: implement Project Reload()

--- internal/config/project.go
+++ internal/config/project.go
@@ -1,6 +1,22 @@
 package config
 
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
 func (p *Project) Reload() error {
-	// TODO:
+	for _, command := range p.Commands {
+		splittedCommand := strings.Split(command.Command, " ")
+		cmd := exec.Command(splittedCommand[0], splittedCommand[1:]...)
+		cmd.Dir = command.WorkDir
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		err := cmd.Run()
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
