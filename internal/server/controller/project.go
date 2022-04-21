3e0f755e470bcc53492da249ba8e54951c6407c3
Paulo <git@pauloo27.dev>
2022-04-20 23:24:57 -0300

    fix: handle of "project not found"
    there's room for improvement tho

--- internal/server/controller/project.go
+++ internal/server/controller/project.go
@@ -10,8 +10,11 @@ func ReloadProject(ctx *fiber.Ctx) error {
 	projectID := ctx.Params("id")
 
 	project, found := config.Projects[projectID]
-	// if not found stuff is done only after the auth is checked to avoid leaking
-	// information about the existence of projects.
+
+	if !found {
+		logger.Info("Cannot found project with ID", projectID)
+		return ctx.SendStatus(fiber.StatusUnauthorized)
+	}
 
 	secretHeader := project.SecretHeader
 	if secretHeader == "" {
@@ -28,11 +31,6 @@ func ReloadProject(ctx *fiber.Ctx) error {
 	if secret != project.Secret {
 		logger.Info("Got a request with an invalid secret")
 		return ctx.SendStatus(fiber.StatusForbidden)
-	}
-
-	if !found {
-		logger.Info("Cannot found project with ID", projectID)
-		return ctx.SendStatus(fiber.StatusNotFound)
 	}
 
 	go func() {
